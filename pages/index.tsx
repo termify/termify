42612dd2bc7b35a5cc94c40b503e10f43d721c95
Eduard Albrecht <eddysoulo91@gmail.com>
2022-08-11 13:46:54 +0200

    change: style a bit

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,11 +9,11 @@ const Home: NextPage = () => {
   const [counter, setCounter] = useState<number>(0) 
 
   return (
-    <div className='flex flex-col gap-8 w-screen h-screen justify-center items-center' >
-      <h1 className='text-3xl font-bold' >Hier kommt eine sexy App... also bald</h1>
-      <div className='flex flex-col items-center gap-4' >
-        <p className='text-xl' > <span className='text-4xl font-bold' >{counter}</span> Personen gefällt diese Nachricht 👍</p>
-        <button onClick={()=>{setCounter(counter+1)}} className='bg-gradient-to-t from-sky-600 to-emerald-400 w-1/3 shadow-lg rounded p-2 text-4xl transition-all hover:scale-110' >👍</button>
+    <div className='container mx-auto p-4 flex flex-col gap-8 w-screen h-screen justify-center items-center' >
+      <h1 className='text-xl font-bold mt-16 text-center md:text-3xl'>Hier kommt eine sexy App... also bald</h1>
+      <div className='flex-grow flex flex-col items-center justify-center gap-4' >
+        <p className='text-xl' > <span className='text-4xl font-bold text-center ' >{counter}</span> Personen gefällt diese Nachricht</p>
+        <button onClick={()=>{setCounter(counter+1)}} className='bg-gradient-to-t from-sky-600 to-emerald-400 w-1/3 shadow-lg rounded p-2 text-4xl transition-all xl:hover:scale-110' >👍</button>
       </div>
     </div>
   )
