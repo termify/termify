226a3b20d874937580d6ad0334d1ba8deb9509ae
ealbrecht <eddysoulo91@gmail.com>
2022-08-13 19:21:43 +0200

    add: Hamburger on small Devices

--- components/shared/footer.tsx
+++ components/shared/footer.tsx
@@ -3,7 +3,7 @@
 
 export default function Footer(){
     return(
-        <footer className="grid grid-cols-2 bg-slate-50 p-4 shadow-inner xl:grid-cols-4" >
+        <footer className="grid grid-cols-2 place-content-center place-items-center bg-slate-50 p-4 shadow-inner xl:grid-cols-4" >
             <div>
                 Infoblock 1
             </div>
--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -1,10 +1,44 @@
 import { AiFillSchedule } from "react-icons/ai";
-
+import { motion } from "framer-motion";
+import { useState } from "react";
 
 export default function Header(){
     return(
-        <header className="bg-slate-50 p-3 shadow-md flex">
+        <header className="bg-slate-50 p-3 shadow-md flex justify-between items-center">
             <h1 className="text-2xl font-bold underline underline-offset-4 flex items-center gap-2 text-slate-900" >Terminapp <AiFillSchedule className="xl:h-12 xl:w-12" /></h1>
+            <Hamburger />
         </header>
+    )
+}
+
+function Hamburger(){
+
+    const [open, setOpen] = useState<boolean>(false);
+
+    return(
+        <div className="w-6 h-6 xl:hidden" onClick={()=>setOpen(value => !value)} >
+            <HamburgerIcon open={open} />
+        </div>
+    )
+}
+
+function HamburgerIcon({open}:{open:boolean}){
+
+
+    return(
+        <div className="h-full flex flex-col justify-around" >
+            <motion.div 
+                animate={open ? {} : {rotateZ:-45,y: 8}}
+                className="bg-slate-900 w-full h-1" 
+            />
+            <motion.div 
+                animate={open ? {} : {opacity:0}}
+                className="bg-slate-900 w-full h-1" /
+            >
+            <motion.div 
+                animate={open ? {} : {rotateZ:45, y:-8}}
+                className="bg-slate-900 w-full h-1" 
+            />
+        </div>
     )
 }
