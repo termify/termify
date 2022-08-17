f0cdf6a33cdd06079aef6a5efd433f01ebac242e
ealbrecht <eddysoulo91@gmail.com>
2022-08-17 16:54:46 +0200

    add: dashboard and schedule as page

---
+++ pages/user/[id]/dashboard/index.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from "next";
+
+//TODO: Dashboard and other pages only visible when logged in
+
+const Dashboard:NextPage = () => {
+    return(
+        <div>
+            Dashboard
+        </div>
+    )
+}
+
+export default Dashboard;
---
+++ pages/user/[id]/schedule/index.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from "next";
+
+
+
+const Schedule:NextPage = () => {
+    return(
+        <div>
+            Hier kommt der Kalendar
+        </div>
+    )
+}
+
+export default Schedule;
