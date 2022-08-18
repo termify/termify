222f026658a7398836f43c5917a94f6760ad7a30
Eduard Albrecht <eddysoulo91@gmail.com>
2022-08-18 11:49:27 +0200

    add: Picked Date now visible

--- components/schedule/schedule.tsx
+++ components/schedule/schedule.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { createContext, useContext, useEffect, useMemo, useState } from "react";
 import ScheduleClass from "../../lib/schedule";
 import { RiArrowDropRightLine, RiArrowDropLeftLine } from "react-icons/ri";
 
@@ -6,10 +6,13 @@ interface Schedule{
     date: ScheduleDate;
 }
 
+const DatePickerContext = createContext<[ScheduleDate,React.Dispatch<React.SetStateAction<ScheduleDate>>] | null>(null);
+
 export default function Schedule({date}:Schedule){
 
     const [gap, setGap] = useState<number>(1);
     const [daysInMonth, setDaysInMonth] = useState<number>(0);
+    const [pickedDay, setPickedDay] = useState<ScheduleDate>({day:1,month:1,year:1900});
 
     useEffect(()=>{
         setGap(new Date(date.year,date.month,1).getDay()-1);
@@ -21,23 +24,25 @@ export default function Schedule({date}:Schedule){
     },[daysInMonth,date.month, date.year,date])
 
     return (
-        <div>
-            <div className="flex w-full my-2" >   
-                {
-                    ["Mo.","Di.","Mi.","Do.","Fr.","Sa.","So."].map((e,i) => <DateDay key={e} dateName={e} isSunday={i === 6} />)
-                }
+        <DatePickerContext.Provider value={[pickedDay, setPickedDay]} >
+            <div>
+                <div className="flex w-full my-2" >   
+                    {
+                        ["Mo.","Di.","Mi.","Do.","Fr.","Sa.","So."].map((e,i) => <DateDay key={e} dateName={e} isSunday={i === 6} />)
+                    }
+                </div>
+                <div className="bg-slate-300/25 flex flex-wrap " >
+                    {
+                        Array.from({length:gap})
+                        .map((e,i) => <PlaceholderDay key={i} />)
+                    }
+                    {
+                        Array.from({length:daysInMonth})
+                        .map((e,i) => <ScheduleDay key={i} dayNumber={i+1} date={date} />)
+                    }
+                </div>
             </div>
-            <div className="bg-slate-300/25 flex flex-wrap " >
-                {
-                    Array.from({length:gap})
-                    .map((e,i) => <ScheduleDay key={i} hidden />)
-                }
-                {
-                    Array.from({length:daysInMonth})
-                    .map((e,i) => <ScheduleDay key={i} dayNumber={i+1} hidden={false} date={date} />)
-                }
-            </div>
-        </div>
+        </DatePickerContext.Provider>
     )
 }
 
@@ -48,20 +53,54 @@ function DateDay({dateName, isSunday}:{dateName:string, isSunday:boolean}){
 }
 
 interface ScheduleDay{
-    dayNumber?: number;
-    hidden: boolean;
-    date?: ScheduleDate;
+    dayNumber: number;
+    date: ScheduleDate;
 }
 
-function ScheduleDay({dayNumber, hidden, date}:ScheduleDay){
+function ScheduleDay({dayNumber, date}:ScheduleDay){
 
-    if (hidden) return <div className="w-[14.2857%] h-14 xl:h-24" />
+    const dateContext = useContext(DatePickerContext);
+    const [pickedDate, setPickedDate] = dateContext as [ScheduleDate,React.Dispatch<React.SetStateAction<ScheduleDate>>];
+    const [picked, setPicked] = useState<boolean>(false);
+
+    const dateChanged = useMemo(()=>{
+
+        setPicked(
+            new Date(pickedDate.year,pickedDate.month,pickedDate.day).toDateString() === 
+            new Date(date.year, date.month, dayNumber).toDateString())
+
+    },[date.month, date.year, dayNumber, pickedDate.day, pickedDate.month,pickedDate.year])
+
+    useEffect(()=>{
+
+    },[dateChanged])
+
+
+    function onClickHandler(e: React.MouseEvent<HTMLDivElement>){
+        setPickedDate({
+            day: dayNumber,
+            month: date.month,
+            year: date.year
+        })
+    }
+
+    const setColor = 
+    picked ? "bg-gradient-to-b from-sky-300 to-sky-500 text-sky-900" :
+    new Date(date?.year as number,date?.month as number,dayNumber).toDateString() === new Date().toDateString() ? 
+    "bg-gradient-to-b from-emerald-300 to-emerald-500 text-emerald-900" : "bg-gradient-to-b from-slate-100 to-slate-300 text-slate-900";
 
     return(
-        <div className={`${new Date(date?.year as number,date?.month as number,dayNumber).toDateString() === new Date().toDateString() ? "bg-gradient-to-b from-emerald-300 to-emerald-500 text-emerald-900" : "bg-gradient-to-b from-slate-100 to-slate-300 text-slate-900"}  w-[14.2857%] h-14 border border-slate-500 flex justify-center items-center text-xl font-bold select-none transition-all xl:hover:scale-110 xl:hover:shadow-xl xl:h-24`} >
+        <div 
+            onClick={onClickHandler}
+            className={
+                `${setColor}  w-[14.2857%] h-14 border border-slate-500 flex justify-center items-center text-xl font-bold select-none transition-all xl:hover:scale-110 xl:hover:shadow-xl xl:h-24`} >
             {dayNumber}
         </div>
     )
+}
+
+function PlaceholderDay(){
+    return <div className="w-[14.2857%] h-14 xl:h-24" />
 }
 
 export function ChangeMonth({date, setDate}:{date:ScheduleDate, setDate:React.Dispatch<React.SetStateAction<ScheduleDate>>}){
@@ -112,5 +151,5 @@ export interface ScheduleDate{
     day: number;
     month: number;
     year: number;
-    dayInWeek: number;
+    dayInWeek?: number;
 }
