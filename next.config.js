d08abb8ecbc2704f68335dfc2c44bbdbca8878ff
Eduard_Albrecht <ealbrecht@palettecad.com>
2022-11-07 13:18:10 +0100

    add new supabase instance

--- next.config.js
+++ next.config.js
@@ -4,10 +4,10 @@ const nextConfig = {
   swcMinify: true,
   env:{
     "APP_NAME":"Termify",
-    "SUPABASE_URL" : "https://snvyhwvjeriizdhzkgud.supabase.co",
-    "SUPABASE_JWT_TOKEN":"cRWBL6R5n3Ol77H61USFpPmF7z//Lrz2/ndzNROANk17qPlvrt0kIB538ddr3wcI7U1wyRgAzPAfkuPwj9Kd7w==",
-    "PUBLIC_KEY":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNudnlod3ZqZXJpaXpkaHprZ3VkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTk0NDc4MTAsImV4cCI6MTk3NTAyMzgxMH0.WarRgkmeIzPc6ZFqp5-Pv8IiKCsZ-EODWwSZwp24YVA",
-    "ANON_KEY":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNudnlod3ZqZXJpaXpkaHprZ3VkIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY1OTQ0NzgxMCwiZXhwIjoxOTc1MDIzODEwfQ.FW-ARW9_MAUgxo4Q1d7kIKNHhEFjNJS5X_ccw2wTWCo"
+    "SUPABASE_URL" : "https://ppnrkwqypmrhtxlxgxla.supabase.co",
+    "SUPABASE_JWT_TOKEN":"aQYiJ1nlbdt+ALwQvQMSADHrKeCL0SthlKPYFyEn4yVsFojMC5HQDi6g+tge+dyKDOgRLhAug5529K8GS1e1Ew==",
+    "PUBLIC_KEY":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBwbnJrd3F5cG1yaHR4bHhneGxhIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njc4MjA5MjcsImV4cCI6MTk4MzM5NjkyN30.UVNjRlgEklDfRcnQuIR9yyCzkwx74IPhYHyBLwX0Few",
+    "ANON_KEY":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBwbnJrd3F5cG1yaHR4bHhneGxhIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY2NzgyMDkyNywiZXhwIjoxOTgzMzk2OTI3fQ.UwIKsyxvSLqRfoxwObB-TPBCzwsbXt6POm4UxA68XoQ"
   }
 }
 
