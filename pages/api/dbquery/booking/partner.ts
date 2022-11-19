841de7e0ba3594dff499a5a183e348e8602138da
Eduard Albrecht <eddysoulo91@gmail.com>
2022-11-19 19:57:34 +0100

    refacftor: Änderungen zurückgestellt

--- pages/api/dbquery/booking/partner.ts
+++ pages/api/dbquery/booking/partner.ts
@@ -38,17 +38,17 @@ const getController = async (req: NextApiRequest, res: NextApiResponse<AllPartne
 const postController = async (req: NextApiRequest, res: NextApiResponse<AllPartnerData[]>) => {
     const { districtId } = req.body as { districtId: number };
 
-    const partnerAllData = (await db.partner.findMany({
-        include: {
-            Office: true,
-            District: true,
-        },
-        where: {
-            districtId, //POST Variable setzen
-        },
-    })) as unknown as AllPartnerData[];
+    // const partnerAllData = (await db.partner.findMany({
+    //     include: {
+    //         Office: true,
+    //         District: true,
+    //     },
+    //     where: {
+    //         districtId, //POST Variable setzen
+    //     },
+    // })) as unknown as AllPartnerData[];
+    //
+    // console.log('Data', partnerAllData);
 
-    console.log('Data', partnerAllData);
-
-    res.status(200).json(partnerAllData);
+    res.status(200).json([]);
 };
