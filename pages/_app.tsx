898f4536c0c0f1a1c266245f281fb5c8a929aed3
Eduard Albrecht <eddysoulo91@gmail.com>
2022-11-20 21:10:22 +0100

    feat: finish first iteration of OG

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,7 +12,25 @@ function MyApp({ Component, pageProps }: AppProps) {
         <>
             <Head>
                 <title>Termify</title>
-                <meta property={'og:image'} content={'http://localhost:3000/api/og'} />
+                <meta name="description" content="Nie mehr Anstehen beim Amt" key="desc" />
+                {/*Facebook Meta Tags*/}
+                <meta property="og:type" content="website" />
+                <meta property={'og:image'} content={'https://develop-termintool.vercel.app/api/og'} />
+                <meta property="og:url" content="https://develop-termintool.vercel.app/" />
+                <meta property="og:description" content="Nie mehr Anstehen beim Amt" />
+                <meta property="og:image" content="https://develop-termintool.vercel.app/api/og'" />
+                <meta property="og:image:secure_url" content="https://develop-termintool.vercel.app/" />
+
+                {/*Twtitter Meta Tags*/}
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta property="twitter:domain" content="https://develop-termintool.vercel.app/" />
+                <meta property="twitter:url" content="https://develop-termintool.vercel.app/" />
+                <meta name="twitter:title" content="Termify" />
+                <meta name="twitter:description" content="Nie mehr Anstehen beim Amt" key="desc" />
+                <meta name="twitter:image" content="https://develop-termintool.vercel.app/api/og'" />
+
+                <meta name="google" content="nositelinkssearchbox" key="sitelinks" />
+                <meta name="google" content="notranslate" key="notranslate" />
             </Head>
             <ClientSideRenderContainer>
                 <div className="min-h-screen flex flex-col">
