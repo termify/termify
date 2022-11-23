f12df4486235ba808a7d656cfa69085618c6976a
KevinBlaeser <blaeser.kevin@gmail.com>
2022-11-23 22:25:54 +0100

    Co-authored-by: Eduard Albrecht <edlixstudios@googlemail.com>

--- pages/api/dbquery/booking/appointment.ts
+++ pages/api/dbquery/booking/appointment.ts
@@ -3,10 +3,12 @@ import { db } from '../../../../lib/database';
 
 type AppointmentData = {
     id: number;
-    district: {
-        id: number;
-        districtName: string;
-    };
+    partnerId: number;
+    userId: string;
+    timestamp: Date;
+    typeOfRequest: string;
+    note: string;
+    attachment: string;
 };
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<AppointmentData[]>) {
@@ -18,19 +20,33 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
 }
 
 const postController = async (req: NextApiRequest, res: NextApiResponse<AppointmentData[]>) => {
-    const { districtId } = req.body as { districtId: number };
+    const {partnerId, userId, timestamp, typeOfRequest, note, attachment} = req.body as {partnerId:number; userId:string; timestamp:Date; typeOfRequest:string; note: string; attachment: string}
 
-    const partnerAllData = (await db.partner.findMany({
-        include: {
-            Office: true,
-            District: true,
-        },
-        where: {
-            districtId, //POST Variable setzen
-        },
-    })) as unknown as AppointmentData[];
+    console.log("Body", req.body);
+    
+    // const appointmentData = (await db.appointment.create({
+    //     data: {
+    //         userId: userId,
+    //         partnerId: partnerId,
+    //         timestamp: timestamp,
+    //         typeOfRequest: typeOfRequest,
+    //         note: note,
+    //         attachment: attachment,
+    //     }
+    // })) as unknown as AppointmentData[];
 
-    console.log('Data', partnerAllData);
+// /*insert into "Appointment" ("userId", "partnerId", "timestamp") 
+// values ('f0e7f64b-210e-435a-afea-de151148e873','1','2022-11-29 10:30:00')*/
+// insert into "AppointmentEntry" ("apId","typeOfRequest","note","attachment") 
 
-    res.status(200).json(partnerAllData);
+// Select id, 'Bürgergeld' as "typeOfRequest", 'meine Notiz' as "note", 
+// 'http://www.link.de/1213' as "attachment" from "Appointment" 
+// where "userId" = 'f0e7f64b-210e-435a-afea-de151148e873' and "partnerId" = '1' and "timestamp" = '2022-11-29 10:30:00';
+
+
+
+    // console.log('Data', appointmentData);
+
+    // res.status(200).json(appointmentData);
+    res.status(200).json({});
 };
