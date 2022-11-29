384b2cb1bb86221e369f9f79d474a58254f60640
Eduard_Albrecht <ealbrecht@palettecad.com>
2022-11-29 08:35:00 +0100

    removed has cookie

--- components/shared/clientSideRenderContainer.tsx
+++ components/shared/clientSideRenderContainer.tsx
@@ -1,25 +1,23 @@
-import { ReactNode, useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
-import { useAuthStore } from '../../store/stores';
-import { hasCookie } from '../../lib/cookie';
+import { ReactNode, useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { useAuthStore } from "../../store/stores";
+import { hasCookie } from "../../lib/cookie";
 
 interface ClientSideRenderContainerProps {
-    children: ReactNode;
+	children: ReactNode;
 }
 
 export default function ClientSideRenderContainer({ children }: ClientSideRenderContainerProps) {
-    const [clientIsReady, setClientIsReady] = useState<boolean>(false);
-    const setLoggedIn = useAuthStore((state) => state.setLoggedIn);
-    const router = useRouter();
+	const [clientIsReady, setClientIsReady] = useState<boolean>(false);
+	// const setLoggedIn = useAuthStore((state) => state.setLoggedIn);
+	const router = useRouter();
 
-    useEffect(() => {
-        if (!router.isReady) return;
+	useEffect(() => {
+		if (!router.isReady) return;
 
-        setLoggedIn(hasCookie('auth'));
-        setClientIsReady(true);
-    }, [router.isReady, setLoggedIn]);
+		// setLoggedIn(hasCookie('auth'));
+		setClientIsReady(true);
+	}, [router.isReady]);
 
-    return <
-    >{clientIsReady ? children : null}</
-    >;
+	return <>{clientIsReady ? children : null}</>;
 }
