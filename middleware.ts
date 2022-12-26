b934d65e5dd435a1f34c1d649db6cbc3c23e2f86
Eduard Albrecht <eddysoulo91@gmail.com>
2022-12-26 19:25:56 +0100

    fix: middleware now dont redirect to login

--- middleware.ts
+++ middleware.ts
@@ -2,13 +2,13 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export async function middleware(req: NextRequest) {
-	const cookie = req.cookies.get("next-auth.session-token");
+	// const cookie = req.cookies.get("next-auth.session-token");
 
 	if (req.nextUrl.pathname === "/") {
 		return NextResponse.redirect(new URL(`/booking`, req.url));
 	}
 
-	if (req.nextUrl.pathname.startsWith("/user")) {
-		if (!cookie) return NextResponse.redirect(new URL("/login", req.url));
-	}
+	// if (req.nextUrl.pathname.startsWith("/user")) {
+	// 	if (!cookie) return NextResponse.redirect(new URL("/login", req.url));
+	// }
 }
