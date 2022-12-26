48fc90e98ac2aed2d7b4e3dd3e5abfa9aa107596
Eduard Albrecht <eddysoulo91@gmail.com>
2022-12-26 18:53:18 +0100

    fix: add ignore linting in og.tsx

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -1,24 +1,28 @@
-import { ImageResponse } from '@vercel/og';
+import { ImageResponse } from "@vercel/og";
 
 export const config = {
-    runtime: 'experimental-edge',
+	runtime: "experimental-edge",
 };
 
 export default function () {
-    return new ImageResponse(<OgImage />, {
-        width: 1200,
-        height: 600,
-        emoji: 'twemoji',
-    });
+	return new ImageResponse(<OgImage />, {
+		width: 1200,
+		height: 600,
+		emoji: "twemoji",
+	});
 }
 
 function OgImage() {
-    return (
-        <div tw={'bg-slate-50 w-screen h-screen flex justify-center items-center flex-col'}>
-            <p tw={'text-7xl font-bold '}>Termify</p>
-            <div tw={'flex'}>
-                <p tw={'text-5xl'}>Terminplanung einfach gemacht </p>
-            </div>
-        </div>
-    );
+	return (
+		// eslint-disable-next-line react/no-unknown-property
+		<div tw={"bg-slate-50 w-screen h-screen flex justify-center items-center flex-col"}>
+			{/* eslint-disable-next-line react/no-unknown-property */}
+			<p tw={"text-7xl font-bold "}>Termify</p>
+			{/* eslint-disable-next-line react/no-unknown-property */}
+			<div tw={"flex"}>
+				{/* eslint-disable-next-line react/no-unknown-property */}
+				<p tw={"text-5xl"}>Terminplanung einfach gemacht </p>
+			</div>
+		</div>
+	);
 }
