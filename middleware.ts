454fc9ac86fc2a50573a08f3e32fbe829877c8fd
Eduard Albrecht <eddysoulo91@gmail.com>
2022-12-31 00:03:07 +0100

    now shamone

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export async function middleware(req: NextRequest) {
-	const cookie = req.cookies;
+	const cookie = req.cookies.get("next-auth.session-token") || req.cookies.get("__Secure-next-auth.session-token");
 
 	console.log("COOKIE FROM MIDDLEWARE", cookie);
 
